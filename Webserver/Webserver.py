2ea634096302af9ac1a0fe5fee1c01334dd7ff4d
MailiLoh <mailina.lohmann@haw-hamburg.de>
2015-11-27 09:46:13 +0100

    2.1
    Funktionsfähig

--- Webserver/Webserver.py
+++ Webserver/Webserver.py
@@ -14,7 +14,7 @@ Val = ("(Zeitstempel, Wert) VALUES (current_timestamp, ")
 app = Flask(__name__)
 
 @app.route("/")
-def hallo(name=None):
+def Start():
     return render_template('Startseite.html')
 
 @app.route("/links")
@@ -30,7 +30,14 @@ def Main():
     cursor.execute("SELECT Wert FROM InTemp WHERE ID=(SELECT Max(ID) FROM InTemp)")
     InTemp = (cursor.fetchone())[0]
     cursor.execute("SELECT Wert FROM Modus WHERE ID=(SELECT Max(ID) FROM Modus)")
-    Modus = (cursor.fetchone())[0]
+    if (cursor.fetchone())[0]:
+        Modus = "automatic"
+        Manu = " "
+        Auto = "disabled"
+    else:
+        Modus = "manuel"
+        Manu = "disabled"
+        Auto = " "
     cursor.execute("SELECT Wert FROM OutHum WHERE ID=(SELECT Max(ID) FROM OutHum)")
     OutHum = (cursor.fetchone())[0]
     cursor.execute("SELECT Wert FROM OutTemp WHERE ID=(SELECT Max(ID) FROM OutTemp)")
@@ -40,11 +47,15 @@ def Main():
     cursor.execute("SELECT Wert FROM StateWindow WHERE ID=(SELECT Max(ID) FROM StateWindow)")
     if (cursor.fetchone())[0]:
         StateWindow = "opend"
+        ManOpen = "disabled"
+        ManClose = " "
     else:
         StateWindow = "closed"
+        ManOpen = " "
+        ManClose = "disabled"
     cursor.execute("SELECT Wert FROM Wind WHERE ID=(SELECT Max(ID) FROM Wind)")
     Wind = (cursor.fetchone())[0]
-    return render_template('Main.html', Modus=Modus, StateWindow=StateWindow, OutTemp=OutTemp, InTemp=InTemp, OutHum=OutHum, InHum=InHum, Air=Air, Wind=Wind, Sound=Sound)
+    return render_template('Main.html', Modus=Modus, StateWindow=StateWindow, ManOpen=ManOpen, ManClose=ManClose, Manu = Manu, Auto = Auto, OutTemp=OutTemp, InTemp=InTemp, OutHum=OutHum, InHum=InHum, Air=Air, Wind=Wind, Sound=Sound)
 
 
 @app.route("/titel")
@@ -102,17 +113,17 @@ def submit():
     cursor.execute(Ins + "MaxWind" + Val + MaxWind +")")
     cursor.execute(Ins + "MaxSound" + Val + MaxSound +")")
     connection.commit()
-    return render_template('Limits-Text.html', MinTemp = MinTemp, MaxTemp = MaxTemp, MinHum = MinHum, MaxHum = MaxHum, MaxAir = MaxAir, MaxWind = MaxWind, MaxSound =MaxSound)
+    return LimitsText()
 
 @app.route("/reset")
 def reset():
-    MinTemp="20"
-    MaxTemp="26"
-    MinHum="30"
-    MaxHum="65"
+    MinTemp="20.0"
+    MaxTemp="26.0"
+    MinHum="30.0"
+    MaxHum="65.0"
     MaxAir="0.1"
     MaxWind="20.8"
-    MaxSound="60"
+    MaxSound="60.0"
     cursor.execute(Ins + "MinTemp" + Val + MinTemp +")")
     cursor.execute(Ins + "MaxTemp" + Val + MaxTemp +")")
     cursor.execute(Ins + "MinHum" + Val + MinHum +")")
@@ -121,7 +132,40 @@ def reset():
     cursor.execute(Ins + "MaxWind" + Val + MaxWind +")")
     cursor.execute(Ins + "MaxSound" + Val + MaxSound +")")
     connection.commit()
-    return render_template('Limits-Text.html', MinTemp = MinTemp, MaxTemp = MaxTemp, MinHum = MinHum, MaxHum = MaxHum, MaxAir = MaxAir, MaxWind = MaxWind, MaxSound =MaxSound)
+    return LimitsText()
+
+@app.route("/open")
+def open():
+    cursor.execute("SELECT Wert FROM Modus WHERE ID=(SELECT Max(ID) FROM Modus)")
+    Modus = (cursor.fetchone())[0]
+    if not (Modus):
+        cursor.execute(Ins + "ManOpen" + Val + "1" +")")
+        cursor.execute(Ins + "ManClose" + Val + "0" +")")    
+        connection.commit()
+    return Main()
+
+
+@app.route("/close")
+def close():
+    cursor.execute("SELECT Wert FROM Modus WHERE ID=(SELECT Max(ID) FROM Modus)")
+    Modus = (cursor.fetchone())[0]
+    if not (Modus):
+        cursor.execute(Ins + "ManOpen" + Val + "0" +")")
+        cursor.execute(Ins + "ManClose" + Val + "1" +")")
+        connection.commit()
+    return Main()
+
+@app.route("/manu")
+def manu():
+    cursor.execute(Ins + "Modus" + Val + "0" +")")
+    connection.commit()
+    return Main()
+
+@app.route("/auto")
+def auto():
+    cursor.execute(Ins + "Modus" + Val + "1" +")")
+    connection.commit()
+    return Main()
 
 
 if __name__ == "__main__":
