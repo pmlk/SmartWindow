469007016ea05bdb751295d1e138a73c820f9780
MailiLoh <mailina.lohmann@haw-hamburg.de>
2015-12-07 09:44:23 +0100

    Anpassung der Variablennamen

--- Webserver/Webserver.py
+++ Webserver/Webserver.py
@@ -23,13 +23,15 @@ def Links():
 
 @app.route("/main")
 def Main():
-    cursor.execute("SELECT Wert FROM Air WHERE ID=(SELECT Max(ID) FROM Air)")
-    Air = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM InHum WHERE ID=(SELECT Max(ID) FROM InHum)")
-    InHum = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM InTemp WHERE ID=(SELECT Max(ID) FROM InTemp)")
-    InTemp = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM Modus WHERE ID=(SELECT Max(ID) FROM Modus)")
+    cursor.execute("SELECT Wert FROM AirPressure_IN WHERE ID=(SELECT Max(ID) FROM AirPressure_IN)")
+    InPres = (cursor.fetchone())[0]
+    cursor.execute("SELECT Wert FROM AirPressure_OUT WHERE ID=(SELECT Max(ID) FROM AirPressure_OUT)")
+    OutPres = (cursor.fetchone())[0]
+    cursor.execute("SELECT Wert FROM AirQuality_IN WHERE ID=(SELECT Max(ID) FROM AirQuality_IN)")
+    InQual = (cursor.fetchone())[0]
+    cursor.execute("SELECT Wert FROM AirQuality_OUT WHERE ID=(SELECT Max(ID) FROM AirQuality_OUT)")
+    OutQual = (cursor.fetchone())[0]
+    cursor.execute("SELECT Wert FROM AutoModus WHERE ID=(SELECT Max(ID) FROM AutoModus)")
     if (cursor.fetchone())[0]:
         Modus = "automatic"
         Manu = " "
@@ -38,13 +40,19 @@ def Main():
         Modus = "manuel"
         Manu = "disabled"
         Auto = " "
-    cursor.execute("SELECT Wert FROM OutHum WHERE ID=(SELECT Max(ID) FROM OutHum)")
+    cursor.execute("SELECT Wert FROM Humidity_IN WHERE ID=(SELECT Max(ID) FROM Humidity_IN)")
+    InHum = (cursor.fetchone())[0]
+    cursor.execute("SELECT Wert FROM Humidity_OUT WHERE ID=(SELECT Max(ID) FROM Humidity_OUT)")
     OutHum = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM OutTemp WHERE ID=(SELECT Max(ID) FROM OutTemp)")
+    cursor.execute("SELECT Wert FROM Temp_IN WHERE ID=(SELECT Max(ID) FROM Temp_IN)")
+    InTemp = (cursor.fetchone())[0]
+    cursor.execute("SELECT Wert FROM Temp_OUT WHERE ID=(SELECT Max(ID) FROM Temp_Out)")
     OutTemp = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM Sound WHERE ID=(SELECT Max(ID) FROM Sound)")
-    Sound = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM StateWindow WHERE ID=(SELECT Max(ID) FROM StateWindow)")
+    cursor.execute("SELECT Wert FROM Volume_IN WHERE ID=(SELECT Max(ID) FROM Volume_IN)")
+    InSound = (cursor.fetchone())[0]
+    cursor.execute("SELECT Wert FROM Volume_OUT WHERE ID=(SELECT Max(ID) FROM Volume_OUT)")
+    OutSound = (cursor.fetchone())[0]
+    cursor.execute("SELECT Wert FROM Win_Open WHERE ID=(SELECT Max(ID) FROM Win_Open)")
     if (cursor.fetchone())[0]:
         StateWindow = "opend"
         ManOpen = "disabled"
@@ -53,9 +61,7 @@ def Main():
         StateWindow = "closed"
         ManOpen = " "
         ManClose = "disabled"
-    cursor.execute("SELECT Wert FROM Wind WHERE ID=(SELECT Max(ID) FROM Wind)")
-    Wind = (cursor.fetchone())[0]
-    return render_template('Main.html', Modus=Modus, StateWindow=StateWindow, ManOpen=ManOpen, ManClose=ManClose, Manu = Manu, Auto = Auto, OutTemp=OutTemp, InTemp=InTemp, OutHum=OutHum, InHum=InHum, Air=Air, Wind=Wind, Sound=Sound)
+    return render_template('Main.html', Modus=Modus, StateWindow=StateWindow, ManOpen=ManOpen, ManClose=ManClose, Manu = Manu, Auto = Auto, OutTemp=OutTemp, InTemp=InTemp, OutHum=OutHum, InHum=InHum, InQual=InQual, OutQual=OutQual, InPres=InPres, OutPres=OutPres, InSound=InSound, OutSound=OutSound)
 
 
 @app.route("/titel")
@@ -80,21 +86,21 @@ def Limits():
 
 @app.route("/limitsText")
 def LimitsText():
-    cursor.execute("SELECT Wert FROM MaxAir WHERE ID=(SELECT Max(ID) FROM MaxAir)")
-    MaxAir = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM MaxHum WHERE ID=(SELECT Max(ID) FROM MaxHum)")
+    cursor.execute("SELECT Wert FROM AirPressure_MAX WHERE ID=(SELECT Max(ID) FROM AirPressure_MAX)")
+    MaxPres = (cursor.fetchone())[0]
+    cursor.execute("SELECT Wert FROM AirQuality_MAX WHERE ID=(SELECT Max(ID) FROM AirQuality_MAX)")
+    MaxQual = (cursor.fetchone())[0]
+    cursor.execute("SELECT Wert FROM Humidity_MAX WHERE ID=(SELECT Max(ID) FROM Humidity_MAX)")
     MaxHum = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM MaxSound WHERE ID=(SELECT Max(ID) FROM MaxSound)")
-    MaxSound = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM MaxTemp WHERE ID=(SELECT Max(ID) FROM MaxTemp)")
-    MaxTemp = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM MaxWind WHERE ID=(SELECT Max(ID) FROM MaxWind)")
-    MaxWind = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM MinHum WHERE ID=(SELECT Max(ID) FROM MinHum)")
+    cursor.execute("SELECT Wert FROM Humidity_MIN WHERE ID=(SELECT Max(ID) FROM Humidity_MIN)")
     MinHum = (cursor.fetchone())[0]
-    cursor.execute("SELECT Wert FROM MinTemp WHERE ID=(SELECT Max(ID) FROM MinTemp)")
+    cursor.execute("SELECT Wert FROM Temp_MAX WHERE ID=(SELECT Max(ID) FROM Temp_MAX)")
+    MaxTemp = (cursor.fetchone())[0]
+    cursor.execute("SELECT Wert FROM Temp_MIN WHERE ID=(SELECT Max(ID) FROM Temp_MIN)")
     MinTemp = (cursor.fetchone())[0]
-    return render_template('Limits-Text.html', MinTemp = MinTemp, MaxTemp = MaxTemp, MinHum = MinHum, MaxHum = MaxHum, MaxAir = MaxAir, MaxWind = MaxWind, MaxSound =MaxSound)
+    cursor.execute("SELECT Wert FROM Volume_MAX WHERE ID=(SELECT Max(ID) FROM Volume_MAX)")
+    MaxSound = (cursor.fetchone())[0]
+    return render_template('Limits-Text.html', MinTemp = MinTemp, MaxTemp = MaxTemp, MinHum = MinHum, MaxHum = MaxHum, MaxQual = MaxQual, MaxPres = MaxPres, MaxSound =MaxSound)
 
 @app.route("/submit", methods=['POST'])
 def submit():
@@ -102,16 +108,16 @@ def submit():
     MaxTemp=request.form['MaxTemp']
     MinHum=request.form['MinHum']
     MaxHum=request.form['MaxHum']
-    MaxAir=request.form['MaxCO2']
-    MaxWind=request.form['MaxWind']
+    MaxQual=request.form['MaxCO2']
+    MaxPres=request.form['MaxPres']
     MaxSound=request.form['MaxNoise']
-    cursor.execute(Ins + "MinTemp" + Val + MinTemp +")")
-    cursor.execute(Ins + "MaxTemp" + Val + MaxTemp +")")
-    cursor.execute(Ins + "MinHum" + Val + MinHum +")")
-    cursor.execute(Ins + "MaxHum" + Val + MaxHum +")")
-    cursor.execute(Ins + "MaxAir" + Val + MaxAir +")")
-    cursor.execute(Ins + "MaxWind" + Val + MaxWind +")")
-    cursor.execute(Ins + "MaxSound" + Val + MaxSound +")")
+    cursor.execute(Ins + "Temp_MIN" + Val + MinTemp +")")
+    cursor.execute(Ins + "Temp_MAX" + Val + MaxTemp +")")
+    cursor.execute(Ins + "Humidity_MIN" + Val + MinHum +")")
+    cursor.execute(Ins + "Humidity_MAX" + Val + MaxHum +")")
+    cursor.execute(Ins + "AirQuality_MAX" + Val + MaxQual +")")
+    cursor.execute(Ins + "AirPressure_MAX" + Val + MaxPres +")")
+    cursor.execute(Ins + "Volume_MAX" + Val + MaxSound +")")
     connection.commit()
     return LimitsText()
 
@@ -124,19 +130,19 @@ def reset():
     MaxAir="0.1"
     MaxWind="20.8"
     MaxSound="60.0"
-    cursor.execute(Ins + "MinTemp" + Val + MinTemp +")")
-    cursor.execute(Ins + "MaxTemp" + Val + MaxTemp +")")
-    cursor.execute(Ins + "MinHum" + Val + MinHum +")")
-    cursor.execute(Ins + "MaxHum" + Val + MaxHum +")")
-    cursor.execute(Ins + "MaxAir" + Val + MaxAir +")")
-    cursor.execute(Ins + "MaxWind" + Val + MaxWind +")")
-    cursor.execute(Ins + "MaxSound" + Val + MaxSound +")")
+    cursor.execute(Ins + "Temp_MIN" + Val + MinTemp +")")
+    cursor.execute(Ins + "Temp_MAX" + Val + MaxTemp +")")
+    cursor.execute(Ins + "Humidity_MIN" + Val + MinHum +")")
+    cursor.execute(Ins + "Humidity_MAX" + Val + MaxHum +")")
+    cursor.execute(Ins + "AirQuality_MAX" + Val + MaxQual +")")
+    cursor.execute(Ins + "AirPressure_MAX" + Val + MaxPres +")")
+    cursor.execute(Ins + "Volume_MAX" + Val + MaxSound +")")
     connection.commit()
     return LimitsText()
 
 @app.route("/open")
 def open():
-    cursor.execute("SELECT Wert FROM Modus WHERE ID=(SELECT Max(ID) FROM Modus)")
+    cursor.execute("SELECT Wert FROM AutoModus WHERE ID=(SELECT Max(ID) FROM AutoModus)")
     Modus = (cursor.fetchone())[0]
     if not (Modus):
         cursor.execute(Ins + "ManOpen" + Val + "1" +")")
@@ -147,7 +153,7 @@ def open():
 
 @app.route("/close")
 def close():
-    cursor.execute("SELECT Wert FROM Modus WHERE ID=(SELECT Max(ID) FROM Modus)")
+    cursor.execute("SELECT Wert FROM AutoModus WHERE ID=(SELECT Max(ID) FROM AutoModus)")
     Modus = (cursor.fetchone())[0]
     if not (Modus):
         cursor.execute(Ins + "ManOpen" + Val + "0" +")")
@@ -157,13 +163,13 @@ def close():
 
 @app.route("/manu")
 def manu():
-    cursor.execute(Ins + "Modus" + Val + "0" +")")
+    cursor.execute(Ins + "AutoModus" + Val + "0" +")")
     connection.commit()
     return Main()
 
 @app.route("/auto")
 def auto():
-    cursor.execute(Ins + "Modus" + Val + "1" +")")
+    cursor.execute(Ins + "AutoModus" + Val + "1" +")")
     cursor.execute(Ins + "ManOpen" + Val + "0" +")")
     cursor.execute(Ins + "ManClose" + Val + "0" +")")
     connection.commit()
