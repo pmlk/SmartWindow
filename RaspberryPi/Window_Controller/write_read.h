c54d50dc51eaa9f1faf964718a5cc1b7e6ab3416
simonsta <simonstallbaum@online.de>
2016-01-07 13:16:38 +0100

    headerfile for write_read.cpp

---
+++ RaspberryPi/Window_Controller/write_read.h
@@ -0,0 +1,19 @@
+/**
+ * written by Simon Stallbaum
+ * header for write_read.cpp
+ */
+#define BUF 255
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "mysql/mysql.h"
+#include "string.h"
+#include "time.h"
+#include "stddef.h"
+
+bool check_error(MYSQL *mysql);
+void connect(MYSQL *mysql);
+void close_session(MYSQL *mysql);
+bool write_in_db(MYSQL *mysql,const char *db, const char *tabelle, const char *value);
+bool get_latest_value_bool(MYSQL *mysql, const char *db, const char *tabelle, bool *succeeded);
+double get_latest_value_double(MYSQL *mysql, const char *db, const char *tabelle, bool *succeeded);
